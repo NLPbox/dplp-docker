5772863f609125ba2cc02a77a63e6b7dbf676420
Arne Neumann <github@arne.cl>
2018-03-31 23:53:46 +0200

    added basic function test

--- Dockerfile
+++ Dockerfile
@@ -3,16 +3,15 @@ FROM nlpbox/corenlp:3.6.0
 # We can't use nltk from the repos, as it has a nltk tree drawing bug
 RUN apt-get update -y && \
     apt-get install -y python-scipy python-numpy python-sklearn python-pip && \
-    pip install nltk==3.2.5
+    pip install nltk==3.2.5 pytest sh
 
 WORKDIR /opt
 # We'll install my fork to produce RST trees as PNG images
 RUN git clone https://github.com/arne-cl/DPLP.git
 
-ADD dplp.sh /opt/DPLP/
-ADD input_long.txt output_break.txt /opt/DPLP/data/
+ADD dplp.sh test_dplp.py /opt/DPLP/
+ADD input_long.txt input_short.txt output_break.txt /opt/DPLP/data/
 ADD output_break.txt /opt/DPLP/
-
 
 WORKDIR /opt/DPLP
 ENTRYPOINT ["./dplp.sh"]
---
+++ test_dplp.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author: Arne Neumann <nlpbox.programming@arne.cl>
+
+import pytest
+import sh
+
+
+EXPECTED_OUTPUT = """0	1	Although	although	IN	mark	3	O	 (ROOT (SBAR (IN Although)	1
+0	2	they	they	PRP	nsubj	3	O	 (S (NP (PRP they))	1
+0	3	didn't	didn't	VBP	root	0	O	 (VP (VBP didn't)	1
+0	4	like	like	IN	case	5	O	 (SBAR (S (PP (IN like)	1
+0	5	it,	it,	NN	nmod	7	O	 (NP (NN it,)))	1
+0	6	they	they	PRP	nsubj	7	O	 (NP (PRP they))	2
+0	7	accepted	accept	VBD	ccomp	3	O	 (VP (VBD accepted)	2
+0	8	the	the	DT	det	9	O	 (NP (DT the)	2
+0	9	offer.	offer.	NN	dobj	7	O	 (NN offer.)))))))))	2
+
+RELATIONS:
+
+((1, 1), 'Nucleus', 'span')
+((2, 2), 'Satellite', 'elaboration')
+"""
+
+
+def test_dplp():
+    """The DPLP parser produces the expected output."""
+    parser = sh.Command('/opt/DPLP/dplp.sh')
+    parser_stdout = parser('/opt/DPLP/data/input_short.txt')
+    assert parser_stdout == EXPECTED_OUTPUT
+
